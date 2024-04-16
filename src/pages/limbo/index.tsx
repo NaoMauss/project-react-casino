f7aaf1d5fd5455ce872f99319b66f715b4461a55
Nao Mausservey <nao.mausservey@sepefrei.fr>
2024-04-16 15:54:22 +0200

    Refactor Limbo page to include user authentication logic and loading state

--- src/pages/limbo/index.tsx
+++ src/pages/limbo/index.tsx
@@ -1,7 +1,33 @@
+// import styles from "@/styles/Limbo.module.css";
+import { useAuthState } from "@/hooks/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 const Limbo = () => {
+    const { user, loading } = useAuthState();
+    const [userInfo, setUserInfo] = useState(null);
+    const router = useRouter();
+
+    useEffect (() => {
+        if (!user && !loading) {
+            console.log(user)
+        }
+        setUserInfo(user);
+    }, [loading, user]);
+
+    if (loading) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        );
+    }
+    
+    
     return (
         <>
             <h1>Limbo </h1>
+            <p>{JSON.stringify(userInfo)} CouCou</p>
         </>
     );
 };
