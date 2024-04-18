211e4bf955645225e3db433d30e7a4f56a046f07
YounesHeysimon <younes@heysimon.fr>
2024-04-18 12:41:59 +0200

    feat(web): styles on limbo page

--- src/pages/limbo/index.tsx
+++ src/pages/limbo/index.tsx
@@ -4,58 +4,75 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Header from "@/component/Header";
 import { getInfoFromDb, getBalance, updateBalance } from "@/firebase";
-
+import { Button, Grid, Stack, TextField } from "@mui/material";
 
 const Limbo = () => {
-    const { user, loading } = useAuthState();
-    const [userInfo, setUserInfo] = useState(null);
-    const [dbInfo, setDbInfo] = useState(null);
-    const [balance, setBalance] = useState<number>(0);
-    const router = useRouter();
+  const { user, loading } = useAuthState();
+  const [userInfo, setUserInfo] = useState(null);
+  const [dbInfo, setDbInfo] = useState(null);
+  const [balance, setBalance] = useState<number>(0);
+  const router = useRouter();
 
-    useEffect (() => {
-        if (!user && !loading) {
-            console.log(user)
-            router.push("/login");
-        }
-
-        getInfoFromDb().then((data) => {
-            console.log(data);
-            setDbInfo(JSON.stringify(data));
-        });
-
-        getBalance().then((data) => {
-            console.log(data);
-            setBalance(data);
-        });
-
-        setUserInfo(user);
-    }, [loading, user, balance]);
-
-    if (loading) {
-        return (
-            <div>
-                <h1>Loading...</h1>
-            </div>
-        );
+  useEffect(() => {
+    if (!user && !loading) {
+      console.log(user);
+      router.push("/login");
     }
 
-    const handleBalance = async () => {
-        const formattedBalance = balance + 100;
-        await updateBalance(formattedBalance);
-        setBalance(formattedBalance);
-    }
-    
-    
+    getInfoFromDb().then((data) => {
+      console.log(data);
+      //@ts-ignore
+      setDbInfo(JSON.stringify(data));
+    });
+
+    getBalance().then((data) => {
+      console.log(data);
+      setBalance(data);
+    });
+
+    setUserInfo(user);
+  }, [loading, user, balance]);
+
+  if (loading) {
     return (
-        <>
-            <Header />
-            <h1>Limbo </h1>
-            <p>{dbInfo}</p>
-            <p>{balance}</p>
-            <button onClick={() => handleBalance()}>Add balance</button>
-        </>
+      <div>
+        <h1>Loading...</h1>
+      </div>
     );
+  }
+
+  const handleBalance = async () => {
+    const formattedBalance = balance + 100;
+    await updateBalance(formattedBalance);
+    setBalance(formattedBalance);
+  };
+
+  return (
+    <>
+      <Header />
+      <Grid container>
+        <Grid item xs={6} sx={{backgroundColor:"rgb(33, 55, 67)"}}>
+          <Stack spacing={2} sx={{padding:"10px"}}>
+            <TextField
+              placeholder="Bet amount"
+              sx={{ backgroundColor: "white", color: "white" }}
+            />
+            <TextField
+              placeholder="Profit on win"
+              disabled
+              sx={{ backgroundColor: "white", color: "white" }}
+            />
+            <Button>Bet</Button>
+          </Stack>
+          <Grid item xs={6} sx={{backgroundColor:"white"}}>
+            <Stack>
+              
+            </Stack>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Limbo;
